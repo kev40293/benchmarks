7a5e0b6b4d1058385bd73ce813cb3d6f80374cbb
Kevin Brandstatter <kjbrandstatter@gmail.com>
2012-09-12 00:31:09 -0500

    TCP server-client bench working seemingly well UDP support works as well
    No threading, killing is clumsy
    server is a forked process and not entirely separate
    Low buffer sizes are really slow compared to 64K

--- netbench.c
+++ netbench.c
@@ -6,10 +6,17 @@
 #include <netinet/in.h>
 #define PORT 10000
 #define ADDRESS "127.0.0.1"
+#define TEST_SIZE 1024LL*1024LL*1024LL
 
 const char * USAGE = "Usage: ./netbench -p [tcp|udp] [-t] -bN\n";
 
-void server(int bsize, int protocol){
+double getTime_usec() {
+   struct timeval tp;
+   gettimeofday(&tp, NULL);
+   return (double) tp.tv_sec * 1E6 + (double)tp.tv_usec;
+}
+
+void run_server(int bsize, int protocol){
    int sock;
    struct sockaddr_in serv;
    sock = socket(AF_INET, protocol, 0);
@@ -24,8 +31,17 @@ void server(int bsize, int protocol){
       perror("Bind Fail/n");
       exit(1);
    }
+   printf("Listening on localhost at port 10000\n");
    listen(sock, 5);
-   while (1){
+   while (protocol == SOCK_DGRAM){
+      struct sockaddr_in udpsock;
+      char * buf;
+      buf = (char*) malloc(sizeof(char)*bsize);
+      int size = sizeof(sock);
+      while (recvfrom(sock, &buf, bsize, 0, (struct sockaddr *)&udpsock, &size) > 0);
+      free(buf);
+   }
+   while (protocol == SOCK_STREAM){
       int addrlen, newsock;
       struct sockaddr_in inadd;
       addrlen = sizeof(struct sockaddr_in);
@@ -36,12 +52,61 @@ void server(int bsize, int protocol){
       }
       char *buffer;
       buffer = (char*)malloc(sizeof(char)*bsize);
-      while(recv(newsock, buffer, bsize, 0) == bsize) {}
-      printf("%s\n", buffer);
+      int rc;
+      //int start, end;
+      //start = getTime_usec();
+      while((rc = recv(newsock, buffer, bsize, 0)) > 0) { }
+      //end = getTime_usec();
+      if (rc < 0){
+         perror("Failed read\n");
+      }
+      /*
+      double netspeed = TEST_SIZE/2E9/(end-start)*8*10E6;
+      netspeed /= (end-start);
+      char unit = 'G';
+      if (netspeed < 1){
+         unit = 'M';
+         netspeed *= 2E10;
+      }
+      //printf("Read %lld in %f milliseconds\n", TEST_SIZE, (end-start)*10E3);
+      //printf("Downspeed: %f %cbits/second\n", netspeed, unit);
+      */
+      free(buffer);
+      close(newsock);
+      exit(0);
    }
+   close(sock);
 }
 
-void client(int bsize, int protocol){
+void run_client(int bsize, int protocol){
+   int sock;
+   struct sockaddr_in serv;
+   sock = socket(AF_INET, protocol, 0);
+   serv.sin_family = AF_INET;
+   serv.sin_port = htons(PORT);
+   inet_pton(AF_INET, ADDRESS, &(serv.sin_addr));
+   if (connect(sock, (struct sockaddr *) &serv, sizeof(serv))< 0){
+      perror("Connect Failed\n");
+      exit(1);
+   }
+   char *buffer;
+   buffer = (char*) malloc(sizeof(char)*bsize);
+   int i;
+   i = TEST_SIZE/bsize;
+   printf("Writing data over network\n");
+   double start,end;
+   start = getTime_usec();
+   for (; i > 0; i--){
+        if (write(sock,buffer, bsize) < 0){
+           perror("Failed write\n");
+           exit(1);
+        }
+   }
+   end = getTime_usec();
+   printf("Write %lld in %f milliseconds\n", TEST_SIZE, (end-start)/1E3);
+   printf("Upspeed: %f Gbits/second\n", TEST_SIZE/2E9*8*1E6/(end-start));
+   close(sock);
+   free(buffer);
 }
 
 int main(int argc, char** argv){
@@ -88,6 +153,15 @@ int main(int argc, char** argv){
    printf("Protocol: %s\n", prot == SOCK_STREAM ? "TCP" : "UDP");
    printf("Threaded: %s\n", threaded ? "Yes": "No");
    printf("Buffer Size: %d\n", bsize);
-   server(bsize, prot);
+   int p;
+   if ((p = fork()) == 0){
+        run_server(bsize, prot);
+   }
+   sleep(2);
+   run_client(bsize, prot);
+   if (prot == SOCK_STREAM)
+        wait(p);
+   else
+      kill(p, 9);
    return 0;
 }
