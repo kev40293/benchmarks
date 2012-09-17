5e9e5ecd587653cb4c13da1dfc87334dce390b85
Kevin Brandstatter <kjbrandstatter@gmail.com>
2012-09-17 10:13:35 -0500

    Created doc file for netbench and added netbench functions to the runbench script

--- runbench.sh
+++ runbench.sh
@@ -9,11 +9,20 @@ fi
 function disk {
 for i in B K M G
 do
-   ./diskbench -r -t -b$i | grep "diskbench"
-   echo ./diskbench -r -b$i | grep "diskbench"
-   echo ./diskbench -t -b$i | grep "diskbench"
-   echo ./diskbench -b$i | grep "diskbench"
+   ./diskbench -r -t -b$i | grep "DISKBENCH"
+   ./diskbench -r -b$i | grep "DISKBENCH"
+   ./diskbench -t -b$i | grep "DISKBENCH"
+   ./diskbench -b$i | grep "DISKBENCH"
 done
 }
 
+function net {
+   for i in 1B 1K 64K
+       do
+          ./netbench -b$i | grep "NETBENCH"
+          ./netbench -b$i -p | grep "NETBENCH"
+          ./netbench -b$i -u | grep "NETBENCH"
+          ./netbench -b$i -u -p| grep "NETBENCH"
+
 disk
+net
